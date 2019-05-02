8208d0a93b03289fc0ae8cc2963fffe8fdc8271b
Aleksandr_Kushniruk <kushniruk_aleksandr@ukr.net>
2019-05-02 15:33:30 +0300

    Globally handling unexpected errors using interceptors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    console.log("Logging the error", error);
+    alert("An unexpected error occured");
+  }
+
+  return Promise.reject(error);
+});
+
 const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 class App extends Component {
   state = {
@@ -51,9 +65,6 @@ class App extends Component {
       //ex.request - succesfully submit a request to the server, otherwise - null
       if (ex.response && ex.response.status === 404) {
         alert("This post has already been deleted.");
-      } else {
-        console.log("Logging the error", ex);
-        alert("An unexpected error occured");
       }
 
       this.setState({ posts: originalPosts });
